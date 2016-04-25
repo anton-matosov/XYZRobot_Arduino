0bb243b25e1c9db7a6502b3247da5b759dc36d2a
Anton Matosov <anton.matosov@gmail.com>
2016-04-25 01:37:56 -0700

    Add joystick class

---
+++ MainProjects/Bolide/BolideJoystick.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Anton Matosov [CTR] on 4/25/16.
+//
+
+#include "BolideJoystick.h"
---
+++ MainProjects/Bolide/BolideJoystick.h
@@ -0,0 +1,11 @@
+//
+// Created by Anton Matosov [CTR] on 4/25/16.
+//
+
+#pragma once
+
+class BolideJoystick
+{
+//    const int horizontal = joystick[0];
+//    const int vertical = joystick[1];
+};
