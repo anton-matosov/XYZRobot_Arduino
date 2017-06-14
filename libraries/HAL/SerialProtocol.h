830a344aa317f4552981b7fa408fe921797b0658
Anton Matosov <anton.matosov@gmail.com>
2017-06-14 00:47:41 -0700

    Add default for serial config

--- libraries/HAL/SerialProtocol.h
+++ libraries/HAL/SerialProtocol.h
@@ -32,7 +32,7 @@
 class SerialProtocol
 {
 public:
-    virtual void begin(const unsigned long baudRate, const uint8_t transferConfig) = 0;
+    virtual void begin(const unsigned long baudRate, const uint8_t transferConfig = SERIAL_8N1) = 0;
     virtual size_t write(uint8_t byte) = 0;
 
     virtual int available() = 0;
--- libraries/HALArduino/ArduinoHardwareSerial.h
+++ libraries/HALArduino/ArduinoHardwareSerial.h
@@ -13,7 +13,7 @@ class ArduinoHardwareSerial : public SerialProtocol
 public:
     ArduinoHardwareSerial(HardwareSerial& arduinoSerial, uint8_t rxdPin);
 
-    void begin(const unsigned long baudRate, const uint8_t transferConfig) override;
+    void begin(const unsigned long baudRate, const uint8_t transferConfig = SERIAL_8N1) override;
     unsigned int write(uint8_t byte) override;
     int available() override;
     int peek() override;
