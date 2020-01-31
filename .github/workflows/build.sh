2dc6f688078cddf5063ac7aee48a13440c8e41ad
Anton Matosov <anton.matosov@gmail.com>
2020-01-31 00:30:12 -0800

    explicit toolchain

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -3,8 +3,10 @@
 set -e
 set -x
 
+ROOT=$PWD
 ARDUINO_SDK_PATH=$PWD/Arduino.app
 mkdir -p build 
 cd build
-cmake -DSDK_PATH_HINTS=$ARDUINO_SDK_PATH ..
+# cmake -DSDK_PATH_HINTS=$ARDUINO_SDK_PATH  ..
+cmake -DSDK_PATH_HINTS=$ARDUINO_SDK_PATH -DCMAKE_TOOLCHAIN_FILE=${ROOT}/cmake/ArduinoToolchain.cmake ..
 cmake --build . --target Bolide
