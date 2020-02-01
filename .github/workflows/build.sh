dc61a30c84a800713e227513125e063b9697a8c7
Anton Matosov <anton.matosov@gmail.com>
2020-02-01 10:26:28 -0800

    fix travis. print more info

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -3,8 +3,16 @@
 set -e
 set -x
 
+curl --version || echo "no curl"
+git --version || echo "no git"
+cmake --version || echo "no cmake"
+make --version || echo "no make"
+ninja --version || echo "no ninja"
+
 ROOT=$PWD
 ARDUINO_SDK_PATH=$PWD/Arduino.app
+cat $ARDUINO_SDK_PATH/Contents/Java/lib/version.txt
+
 mkdir -p build 
 cd build
 # cmake -DSDK_PATH_HINTS=$ARDUINO_SDK_PATH  ..
